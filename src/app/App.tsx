889013bdb8faa6bffee9c8f39afdd9cfed9e3667
Mia-Salazar <maria.sgr@gmail.com>
2025-06-09 11:31:13 +0200

    Add router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,26 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { Home } from "../presentation/pages";
+
+const routes = [
+  {
+    path: '/',
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/characters" />,
+      },
+      {
+        path: '/characters',
+        element: <Home />,
+      }
+    ],
+  },
+];
+
 function App() {
   return (
-    <>
-      hola
-    </>
-  )
+    <RouterProvider router={createBrowserRouter(routes)} />
+  );
 }
 
-export default App
+export default App;
