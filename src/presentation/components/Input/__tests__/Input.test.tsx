2984e1cad84183bb84908276a4c1d7fce2d0471f
Mia-Salazar <maria.sgr@gmail.com>
2025-06-10 18:18:43 +0200

    Add tests to input

---
+++ src/presentation/components/Input/__tests__/Input.test.tsx
@@ -0,0 +1,76 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Input from '../Input';
+
+describe('Input Component', () => {
+    test('should show label', () => {
+        const handleChange = vi.fn()
+        render(
+            <Input
+                hasLabel={true}
+                id="name"
+                onChange={handleChange}
+                placeholder="This is a placeholder"
+                text="Name" 
+                value=""
+            />
+            );
+
+        const label = screen.getByText(/Name/i);
+        expect(label).toBeVisible();
+    });
+
+    test('should not show label', () => {
+        const handleChange = vi.fn()
+        render(
+            <Input
+                hasLabel={false}
+                id="name"
+                onChange={handleChange}
+                placeholder="This is a placeholder"
+                text="Name" 
+                value=""
+            />
+        );
+
+        const label = screen.queryByText(/Name/i);
+        expect(label).not.toBeInTheDocument();
+    });
+
+    test('should show placeholder', () => {
+        const handleChange = vi.fn()
+        render(
+            <Input
+                hasLabel={false}
+                id="name"
+                onChange={handleChange}
+                placeholder="This is a placeholder"
+                text="Name" 
+                value=""
+            />
+        );
+
+        const placeholder = screen.getByPlaceholderText('This is a placeholder')
+        expect(placeholder).toBeVisible();
+    });
+
+    test('should fire onChange', async () => {
+        const handleChange = vi.fn()
+        render(
+            <Input
+                hasLabel={true}
+                id="name"
+                onChange={handleChange}
+                placeholder="This is a placeholder"
+                text="Name" 
+                value=""
+            />
+        );
+
+        const label = screen.getByText(/Name/i);
+        await userEvent.type(label, "hello")
+
+        expect(handleChange).toHaveBeenCalled()
+    });
+});
