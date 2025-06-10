9becd797dd8a85354d27a5901e01ab3b0ff5ba20
Mia-Salazar <maria.sgr@gmail.com>
2025-06-10 13:34:36 +0200

    Add Loading

--- src/presentation/pages/Results/Results.tsx
+++ src/presentation/pages/Results/Results.tsx
@@ -1,9 +1,20 @@
 import { useSearchParams } from "react-router-dom";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import { Item, Layout } from "../../components";
 import { useCharacterFilter } from "../../hooks/useCharacterFiltered";
 
 import "./Results.css"
+
+const Loading = () => {
+    return (
+        <figure aria-hidden="true">
+            <FontAwesomeIcon icon={faSpinner} size="xs" />
+        </figure>
+    )
+}
 
 const Results = () => {
     const [searchParams] = useSearchParams();
@@ -12,17 +23,19 @@ const Results = () => {
     const to = Number(searchParams.get('to'));
 
     const { data, isLoading } = useCharacterFilter(from, to, name)
-
-    if (isLoading) return <div>Loading</div>
-
-    console.log(data, 'data')
-
     return (
         <Layout>
             <h2>{data?.length} results found:</h2>
-            <ul className="list">
-                {data?.map(({id, image, ki, name, description}) => <Item key={id} name={name} image={image} ki={ki} description={description} />)}
-            </ul>
+
+            <div aria-busy={isLoading ? true : false} aria-live="polite">
+                {isLoading ? <Loading /> :
+                    (
+                        <ul className="list">
+                            {data?.map(({id, image, ki, name, description}) => <Item key={id} name={name} image={image} ki={ki} description={description} />)}
+                        </ul>
+                    )
+                }
+            </div>
         </Layout>
     );
 };
